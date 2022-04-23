9f3a61ca96b72541a1b45edd545d3da98294c1d0
Jorian Olk-Szost <jolkszost@gmail.com>
2022-04-23 17:18:46 -0400

    add calculator functions so works with = sign

--- script.js
+++ script.js
@@ -1,10 +1,94 @@
-const add = function(num1, num2) {
-	addFunc = num1 + num2;
+let displayValue = document.getElementById('display');
+let num1Arr = [];
+let num2Arr = [];
+let operandArr = [];
+
+
+ /*   function numberID(clickedNumber) {
+        console.log(clickedNumber)
+     // displayValue.textContent = clickedNumber;
+     let displayNumber = document.createTextNode(clickedNumber);
+    displayValue.appendChild(displayNumber);
+        num1Arr.push(clickedNumber);
+        
+    }
+    
+
+console.log(num2Arr);*/
+
+function operandID(clickedOperand) {
+    if (num2Arr < 1) {
+    displayValue.textContent='';
+    operandArr.push(clickedOperand);
+    } 
+}
+
+    console.log(operandArr);
+    
+
+function numberID(clickedNumber) {
+    console.log(clickedNumber)
+ // displayValue.textContent = clickedNumber;
+ if (operandArr.length === 0) {
+ let displayNumber = document.createTextNode(clickedNumber);
+displayValue.appendChild(displayNumber);
+    num1Arr.push(clickedNumber);
+ } else if (operandArr.length === 1) {
+    let displayNumber = document.createTextNode(clickedNumber);
+    displayValue.appendChild(displayNumber);
+        num2Arr.push(clickedNumber);
+
+ }
+}
+
+
+function clearID(clickedClear) {
+    console.log(clickedClear)
+    displayValue.textContent = '';
+    while(operandArr.length > 0) {
+        operandArr.pop();
+    } while (num1Arr > 0) {
+        num1Arr.pop();
+    } while (num2Arr > 0) {
+        num2Arr.pop()
+    }
+    return clickedClear;
+}
+
+function equals() {
+    
+    displayValue.textContent = '';
+    let num1 = Number(num1Arr.join(""));
+    let num2 = Number(num2Arr.join(""));
+    let operand = operandArr.toString();
+    let result = operate(operand, num1, num2);
+     let displayNumber = document.createTextNode(result);
+     displayValue.appendChild(displayNumber);
+    while(operandArr.length > 0) {
+         operandArr.pop();
+     } while (num1Arr > 0) {
+         num1Arr.pop();
+     } while (num2Arr > 0) {
+         num2Arr.pop()
+     }
+     num1Arr.push(result);
+}
+
+
+
+
+
+
+
+
+
+const add = function(x, y) {
+	addFunc = x + y;
   return addFunc;
 };
 
-const subtract = function(num1, num2) {
-	subFunc = num1 - num2;
+const subtract = function(x, y) {
+	subFunc = x - y;
   return subFunc;
 };
 /*const multiply = function(numb) {
@@ -15,14 +99,15 @@ const subtract = function(num1, num2) {
       );
       return Number(multFunc);
     };  */
-const divide = function(num1, num2) {
-    divFunc = num1 / num2
+const divide = function(x, y) {
+    divFunc = x / y;
     return divFunc;
 };
-const multiply = function(num1, num2) {
-    multFunc = num1 * num2;
+const multiply = function(x, y) {
+    multFunc = x * y;
     return multFunc;
 };
+
 
 function operate(operand, num1, num2) {
     
@@ -35,22 +120,10 @@ function operate(operand, num1, num2) {
     } else if (operand === '/') {
         return divide(num1, num2)
     }
+    console.log(operate())
 };
 
 
-function operandID(clickedOperand) {
-    console.log(clickedOperand)
-    return clickedOperand;
-}
-function numberID(clickedNumber) {
-    console.log(clickedNumber)
-    return clickedNumber;
-}
-
-function clearID(clickedClear) {
-    console.log(clickedClear)
-    return clickedClear;
-}
-
-let displayValue = document.getElementById('display');
+//let operand = clickedOperand;
+//let num2 = clickedNumber;
 
