21585a886af43fd440957ddb5cebc178f3f29238
Jorian Olk-Szost <jolkszost@gmail.com>
2022-04-25 13:27:58 -0400

    debug operand, number, equals functions

--- script.js
+++ script.js
@@ -2,34 +2,24 @@ let displayValue = document.getElementById('display');
 let num1Arr = [];
 let num2Arr = [];
 let operandArr = [];
+let resultArr = [];
 
 
 
- /*   function numberID(clickedNumber) {
-        console.log(clickedNumber)
-     // displayValue.textContent = clickedNumber;
-     let displayNumber = document.createTextNode(clickedNumber);
-    displayValue.appendChild(displayNumber);
-        num1Arr.push(clickedNumber);
-        
-    }
-    
-
-console.log(num2Arr);*/
 
 function operandID(clickedOperand) {
   
-    if (num2Arr.length === 0) {
- //   displayValue.textContent='';
+    if (num2Arr.length === 0 && operandArr.length === 0) {
+
     operandArr.push(clickedOperand);
     }
- else if (num2Arr.length > 0) {
+ else if (num2Arr.length > 0 && operandArr.length === 1) {
 
     displayValue.textContent = '';
     let num1 = Number(num1Arr.join(""));
     let num2 = Number(num2Arr.join(""));
     let operand = operandArr.toString();
-  //  let resultArr = [];
+  
     let result = operate(operand, num1, num2);
      let displayNumber = document.createTextNode(result);
      displayValue.appendChild(displayNumber);
@@ -40,12 +30,33 @@ function operandID(clickedOperand) {
     }  while (num2Arr.length > 0) {
          num2Arr.pop()
      }
-  //   resultArr.push(result);
+ 
      num1Arr.push(result);
      operandArr.push(clickedOperand);
 
-  //   while (resultArr
+ 
      
+} else if (num2Arr.length === 0 && operandArr.length === 1 && num1Arr.length > 0) {
+    num2Arr.push(num1Arr);
+    displayValue.textContent = '';
+    let num1 = Number(num1Arr.join(""));
+    let num2 = Number(num2Arr.join(""));
+    let operand = operandArr.toString();
+  
+    let result = operate(operand, num1, num2);
+     let displayNumber = document.createTextNode(result);
+     displayValue.appendChild(displayNumber);
+  while(operandArr.length > 0) {
+        operandArr.pop();}
+      while (num1Arr.length > 0) {
+         num1Arr.pop();
+    }  while (num2Arr.length > 0) {
+         num2Arr.pop()
+     }
+ 
+     num1Arr.push(result);
+     operandArr.push(clickedOperand);
+
 }
 }
     console.log(operandArr);
@@ -53,26 +64,32 @@ function operandID(clickedOperand) {
 
 function numberID(clickedNumber) {
     console.log(clickedNumber)
- // displayValue.textContent = clickedNumber;
- if (operandArr.length === 0 && num1Arr.length === 0) {
+
+ if (operandArr.length === 0) {
  let displayNumber = document.createTextNode(clickedNumber);
 displayValue.appendChild(displayNumber);
     num1Arr.push(clickedNumber);
- } else if (operandArr.length === 1 && num2Arr.length === 0) {
-     displayValue.textContent = ''
+ } else if (operandArr.length === 1) {
+     if (num2Arr.length === 0) {
+         displayValue.textContent = ''
     let displayNumber = document.createTextNode(clickedNumber);
     displayValue.appendChild(displayNumber);
         num2Arr.push(clickedNumber);
 
- } 
-     
+ } else {
+    let displayNumber = document.createTextNode(clickedNumber);
+    displayValue.appendChild(displayNumber);
+        num2Arr.push(clickedNumber);
+
+ }
+}    
 }
 
 
 
 
 function equals() {
-    
+    if (num1Arr.length > 0 && num2Arr.length > 0 && operandArr.length > 0) {
     displayValue.textContent = '';
     let num1 = Number(num1Arr.join(""));
     let num2 = Number(num2Arr.join(""));
@@ -82,27 +99,18 @@ function equals() {
      displayValue.appendChild(displayNumber);
   while(operandArr.length > 0) {
         operandArr.pop();}
-      while (num1Arr > 0) {
+      while (num1Arr.length > 0) {
          num1Arr.pop();
-    }  while (num2Arr > 0) {
+    }  while (num2Arr.length > 0) {
          num2Arr.pop()
      }
      num1Arr.push(result);
+} else 
+displayValue.textContent = num1Arr;
 }
 
-
-
-function clearID(clickedClear) {
-    console.log(clickedClear)
-    displayValue.textContent = '';
-    while(operandArr.length > 0) {
-        operandArr.pop();
-    } while (num1Arr > 0) {
-        num1Arr.pop();
-    } while (num2Arr > 0) {
-        num2Arr.pop()
-    }
-    return clickedClear;
+function percent() {
+    
 }
 
 
@@ -118,17 +126,24 @@ const subtract = function(x, y) {
 	subFunc = x - y;
   return subFunc;
 };
-/*const multiply = function(numb) {
-    
-      const multFunc = numb.reduce(
-        (previousValue, currentValue) =>
-        Number(previousValue) * Number(currentValue)
-      );
-      return Number(multFunc);
-    };  */
+
 const divide = function(x, y) {
-    divFunc = x / y;
+    if (x === 0 || y === 0) {
+     while(operandArr.length > 0) {
+            operandArr.pop();
+        } while (num1Arr.length > 0) {
+            num1Arr.pop();
+        } while (num2Arr.length > 0) {
+            num2Arr.pop()
+        }
+        displayValue.textContent = 'EAT A DICK!';
+        
+       
+    } else {
+        divFunc = x / y;
     return divFunc;
+       
+    }
 };
 const multiply = function(x, y) {
     multFunc = x * y;
@@ -137,20 +152,21 @@ const multiply = function(x, y) {
 
 
 function operate(operand, num1, num2) {
-    
+    if (num1 === undefined || num2 === undefined || operand == undefined) {
+        return NaN;
+    } else {
     if (operand === '+') {
-        return add(num1, num2)
+        return add(num1, num2);
     } else if (operand === '-') {
-        return subtract(num1,num2)
+        return subtract(num1,num2);
     } else if (operand === '*') {
-        return multiply(num1, num2)
+        return multiply(num1, num2);
     } else if (operand === '/') {
-        return divide(num1, num2)
+        return divide(num1, num2);
     }
-    console.log(operate())
+}
+ //   console.log(operate())
 };
 
 
-//let operand = clickedOperand;
-//let num2 = clickedNumber;
 
