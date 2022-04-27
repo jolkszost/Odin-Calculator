248b5e4b99668d4b634b3895eef3a2cc329aee9b
Jorian Olk-Szost <jolkszost@gmail.com>
2022-04-27 14:56:10 -0400

    add style, change display to string, limit chars

--- script.js
+++ script.js
@@ -2,7 +2,11 @@ let displayValue = document.getElementById('display');
 let num1Arr = [];
 let num2Arr = [];
 let operandArr = [];
-let resultArr = [];
+let num1Str = '';
+let num2Str = '';
+
+displayValue.textContent ='0';
+ 
 
 
 
@@ -59,32 +63,56 @@ function operandID(clickedOperand) {
 
 }
 }
-    console.log(operandArr);
+ //   console.log(operandArr);
     
 
 function numberID(clickedNumber) {
     console.log(clickedNumber)
-
- if (operandArr.length === 0) {
- let displayNumber = document.createTextNode(clickedNumber);
-displayValue.appendChild(displayNumber);
+ if  (displayValue.textContent === '0') {
+    displayValue.textContent = clickedNumber;
+ //  num1Str.padEnd(num1Str.length + 1, clickedNumber.toString());
+ //   displayValue.appendChild(num1Str);
     num1Arr.push(clickedNumber);
+  //  let num1 = Number(num1Arr.join(""));
+  //  displayValue.textContent = num1;
+ } else {
+ if (operandArr.length === 0) {
+    if (num1Arr.length >= 9) {
+        num1Arr.length === 9;
+        return;
+    } else
+ //   displayValue.appendChild(num1Str);
+  
+ //let displayNumber = document.createTextNode(clickedNumber);
+   // displayValue.appendChild(displayNumber);
+    num1Arr.push(clickedNumber);
+    let num1 = Number(num1Arr.join(""));
+    displayValue.textContent = num1;
+   
+    
  } else if (operandArr.length === 1) {
      if (num2Arr.length === 0) {
-         displayValue.textContent = ''
+   /*      displayValue.textContent = '';
     let displayNumber = document.createTextNode(clickedNumber);
-    displayValue.appendChild(displayNumber);
+    displayValue.appendChild(displayNumber); */
+    displayValue.textContent = clickedNumber;
         num2Arr.push(clickedNumber);
 
  } else {
-    let displayNumber = document.createTextNode(clickedNumber);
-    displayValue.appendChild(displayNumber);
-        num2Arr.push(clickedNumber);
+   /* let displayNumber = document.createTextNode(clickedNumber);
+    displayValue.appendChild(displayNumber);*/
+    if (num2Arr.length >= 9) {
+        num2Arr.length === 9;
+        return;
+    } 
+    num2Arr.push(clickedNumber);
+    let num2 = Number(num2Arr.join(""));
+    displayValue.textContent = num2;
 
  }
 }    
 }
-
+}
 
 
 
@@ -95,8 +123,11 @@ function equals() {
     let num2 = Number(num2Arr.join(""));
     let operand = operandArr.toString();
     let result = operate(operand, num1, num2);
-     let displayNumber = document.createTextNode(result);
-     displayValue.appendChild(displayNumber);
+    if (result.length > 9) {
+        result.length === 9
+        return result;
+    };
+     displayValue.textContent = result;
   while(operandArr.length > 0) {
         operandArr.pop();}
       while (num1Arr.length > 0) {
@@ -109,9 +140,14 @@ function equals() {
 displayValue.textContent = num1Arr;
 }
 
-function percent() {
+/*function negPos() {
+    if (num1Arr.indexOf('-') === -1) {
+        num1Arr.unshift('-');
+        displayValue.textContent = num1Arr.join('');
+    }
+
     
-}
+}*/
 
 
 
@@ -128,22 +164,9 @@ const subtract = function(x, y) {
 };
 
 const divide = function(x, y) {
-    if (x === 0 || y === 0) {
-     while(operandArr.length > 0) {
-            operandArr.pop();
-        } while (num1Arr.length > 0) {
-            num1Arr.pop();
-        } while (num2Arr.length > 0) {
-            num2Arr.pop()
-        }
-        displayValue.textContent = 'EAT A DICK!';
-        
-       
-    } else {
-        divFunc = x / y;
+
+     divFunc = x / y;
     return divFunc;
-       
-    }
 };
 const multiply = function(x, y) {
     multFunc = x * y;
@@ -152,21 +175,28 @@ const multiply = function(x, y) {
 
 
 function operate(operand, num1, num2) {
-    if (num1 === undefined || num2 === undefined || operand == undefined) {
+ /*   if (num1 === undefined || num2 === undefined || operand == undefined) {
         return NaN;
-    } else {
+    } else { */
     if (operand === '+') {
         return add(num1, num2);
     } else if (operand === '-') {
         return subtract(num1,num2);
     } else if (operand === '*') {
         return multiply(num1, num2);
-    } else if (operand === '/') {
+    } else if (operand === '/' && num1 !==0 && num2 !== 0) {
         return divide(num1, num2);
-    }
+    } else 
+    while(operandArr.length > 0) {
+        operandArr.pop();
+    } while (num1Arr.length > 0) {
+        num1Arr.pop();
+    } while (num2Arr.length > 0) {
+        num2Arr.pop()
+    return  'eat a dick';
 }
  //   console.log(operate())
 };
-
+console.log(num1Arr, num2Arr, operandArr);
 
 
